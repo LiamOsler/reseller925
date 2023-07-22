95abbb541686857ef4b00a13f13cf50171c465cc
Liam Osler <osler.liam@gmail.com>
2023-07-22 18:39:02 -0300

    update parts

--- routes/parts.js
+++ routes/parts.js
@@ -5,7 +5,11 @@ var db = require('../database/db');
 
 /* GET all parts */
 router.get('/', function(req, res, next) {
-    db.any(`SELECT part_number, part_name, part_description FROM public.parts925`, [true])
+    // Get the company-specific table name based on the company ID
+    var companyTable = getCompanyTable(req.query.company_id);
+
+    // Query the database to fetch all parts from the appropriate table
+    db.any(`SELECT part_number, part_name, part_description FROM public.${companyTable}`)
         .then(function(data) {
             res.json(data);
         })
@@ -14,19 +18,25 @@ router.get('/', function(req, res, next) {
         });
 });
 
+/* POST a new part */
 router.post('/', function(req, res, next) {
+    // Extract the data from the request body
     var partName = req.body.part_name;
     var partDescription = req.body.part_description;
     var quantityOnHand = req.body.quantity_on_hand;
+    var companyId = req.body.company_id;
 
-    console.log(partName, partDescription, quantityOnHand);
+    // Get the company-specific table name based on the company ID
+    var companyTable = getCompanyTable(companyId);
 
+    console.log(partName, partDescription, quantityOnHand, companyId);
+
+    // Insert the new part into the appropriate company-specific table
     db.any(`
-        INSERT INTO public.parts925
+        INSERT INTO public.${companyTable}
         (created_at, part_name, part_description, quantity_on_hand)
         VALUES(now(), $1, $2, $3)
-        RETURNING part_number, part_name, part_description, quantity_on_hand
-        ;
+        RETURNING part_number, part_name, part_description, quantity_on_hand;
     `, [partName, partDescription, quantityOnHand])
     .then(function(data) {
         console.log(data);
@@ -38,14 +48,19 @@ router.post('/', function(req, res, next) {
     });
 });
 
-
 /* GET parts by part number */
 router.get('/number/:number', function(req, res, next) {
+    // Extract the part number and company ID from the request
     var partNumber = req.params.number;
+    var companyId = req.query.company_id;
 
+    // Get the company-specific table name based on the company ID
+    var companyTable = getCompanyTable(companyId);
+
+    // Query the database to fetch the part with the given part number from the appropriate table
     db.any(`
         SELECT part_number, part_name, part_description 
-        FROM public.parts925 
+        FROM public.${companyTable}
         WHERE "part_number" = $1 
     `, [partNumber])
         .then(function(data) {
@@ -57,14 +72,21 @@ router.get('/number/:number', function(req, res, next) {
     });
 });
 
+/* UPDATE a part by part number */
 router.put('/number/:number', function(req, res, next) {
+    // Extract the part number and company ID from the request
     var partNumber = req.params.number;
     var partName = req.body.part_name;
     var partDescription = req.body.part_description;
     var quantityOnHand = req.body.quantity_on_hand;
+    var companyId = req.body.company_id;
 
+    // Get the company-specific table name based on the company ID
+    var companyTable = getCompanyTable(companyId);
+
+    // Update the part in the appropriate company-specific table based on the part number
     db.any(`
-        UPDATE public.parts925
+        UPDATE public.${companyTable}
         SET part_name=$2, part_description=$3, quantity_on_hand=$4
         WHERE part_number=$1
         RETURNING part_number, part_name, part_description, quantity_on_hand;
@@ -78,15 +100,20 @@ router.put('/number/:number', function(req, res, next) {
     });
 });
 
+/* DELETE a part by part number */
 router.delete('/number/:number', function(req, res, next) {
+    // Extract the part number and company ID from the request
     var partNumber = req.params.number;
+    var companyId = req.query.company_id;
 
+    // Get the company-specific table name based on the company ID
+    var companyTable = getCompanyTable(companyId);
+
+    // Delete the part from the appropriate company-specific table based on the part number
     db.any(`
-        DELETE FROM public.parts925
+        DELETE FROM public.${companyTable}
         WHERE part_number=$1
-        RETURNING part_number, part_name, part_description, quantity_on_hand
-
-        ;
+        RETURNING part_number, part_name, part_description, quantity_on_hand;
     `, [partNumber])
     .then(function(data) {
         res.json(data);
@@ -96,5 +123,19 @@ router.delete('/number/:number', function(req, res, next) {
         res.json(error);
     }); 
 });
+
+// Function to get the company-specific table name based on the company name
+function getCompanyTable(companyName) {
+    // Define the mapping of company names to table prefixes here.
+    // For example:
+    const companyTableMap = {
+        "X": "X_parts925",
+        "Y": "Y_parts925",
+        // Add more entries as needed for other companies.
+    };
+
+    // Return the table name based on the company name, or use a default table name if not found
+    return companyTableMap[companyName.toUpperCase()] || "parts925";
+}
 
 module.exports = router;
